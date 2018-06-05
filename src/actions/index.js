48ee3a8523c561e9069cf5c3ce1b70141ac275ea
eric-dl <31330964+eric-dl@users.noreply.github.com>
2018-06-04 22:39:08 -0500

    4/15

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,7 +13,7 @@ export { selectBook };
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 const API_KEY = "00544fb04d5064510bbed67f1988f1f2";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
 
 const fetchWeather = (city) => {
     const URL = `${ROOT_URL}&q=${city},us`;
